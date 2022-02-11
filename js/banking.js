2515805bb57f0d936505d257615406f2e11a66d2
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-02-11 12:00:21 +0600

    use function

--- js/banking.js
+++ js/banking.js
@@ -1,40 +1,51 @@
-// deposite button add event listener
-document.getElementById('deposit-btn').addEventListener('click', function () {
-    const depositField = document.getElementById('user-deposit-input');
+function getInputValue(userInputId) {
+    const depositField = document.getElementById(userInputId);
     let depositAmountInput = depositField.value;
-    const totalDeposit = document.getElementById('total-deposit');
-    let perviousTotalDeposit = totalDeposit.innerText;
-    let totalDepositAmount = parseFloat(perviousTotalDeposit) + parseFloat(depositAmountInput);
-    totalDeposit.innerText = totalDepositAmount;
+    let newBalDip = parseFloat(depositAmountInput);
 
     depositField.value = '';
+
+    return newBalDip;
+}
+
+// deposite button add event listener
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    // const depositField = document.getElementById('user-deposit-input');
+    // let depositAmountInput = depositField.value;
+    const totalDeposit = document.getElementById('total-deposit');
+    let perviousTotalDeposit = totalDeposit.innerText;
+    let newDeposit = getInputValue('user-deposit-input');
+    let totalDepositAmount = parseFloat(perviousTotalDeposit) + newDeposit;
+    totalDeposit.innerText = totalDepositAmount;
+
+    // depositField.value = '';
 
     const totalBalance = document.getElementById('total-balance');
 
     let previousTotalBalance = totalBalance.innerText;
-
-    let newTotalBalance = parseFloat(previousTotalBalance) + parseFloat(depositAmountInput);
+    let newTotalBalance = parseFloat(previousTotalBalance) + newDeposit;
     totalBalance.innerText = newTotalBalance;
 
 });
 
-// withdraw button add event listener
 
+// withdraw button add event listener
 document.getElementById('withdraw-btn').addEventListener('click', function () {
-    const depositField = document.getElementById('user-withdraw-input');
-    let depositAmountInput = depositField.value;
+    // const depositField = document.getElementById('user-withdraw-input');
+    // let depositAmountInput = depositField.value;
     const totalDeposit = document.getElementById('total-withdraw');
     let perviousTotalDeposit = totalDeposit.innerText;
-    let totalDepositAmount = parseFloat(perviousTotalDeposit) + parseFloat(depositAmountInput);
+    let newWithdraw = getInputValue('user-withdraw-input');
+    let totalDepositAmount = parseFloat(perviousTotalDeposit) + newWithdraw;
     totalDeposit.innerText = totalDepositAmount;
 
-    depositField.value = '';
+    // depositField.value = '';
 
     const totalBalance = document.getElementById('total-balance');
 
     let previousTotalBalance = totalBalance.innerText;
 
-    let newTotalBalance = parseFloat(previousTotalBalance) - parseFloat(depositAmountInput);
+    let newTotalBalance = parseFloat(previousTotalBalance) - newWithdraw;
     totalBalance.innerText = newTotalBalance;
 
 });
